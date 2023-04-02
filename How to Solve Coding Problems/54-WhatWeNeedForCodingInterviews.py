e1c2e25dc95540a8cc7a6ee105493928b12fb03e
amgartendev <joaoamgarten2003@gmail.com>
2023-04-02 14:57:57 +0100

    second commit

---
+++ How to Solve Coding Problems/54-WhatWeNeedForCodingInterviews.py
@@ -0,0 +1,21 @@
+"""
+In order to solve coding problems, we need to learn these data structures and these algorithms:
+
+Data Structures:
+- Arrays
+- Stacks
+- Queues
+- Linked Lists
+- Trees
+- Tries
+- Graphs
+- Hash Tables
+
+Algorithms:
+- Sorting
+- Dynamic Programming
+- BFS + DFS (Searching)
+- Recursion
+
+There are a lot more of data structures and algorithms to learn, but let's focus what really matters for now.
+"""
---
+++ How to Solve Coding Problems/55-Exercise_GoogleInterview.py
@@ -0,0 +1,17 @@
+"""
+Watch this video: https://www.youtube.com/watch?v=XKu_SEDAykw&ab_channel=LifeatGoogle
+
+It is a Google interview for a Software Engineer problem.
+When you watchi this, it's going to be really, really hard to follow the actual coding, maybe some of the languages
+that they use. But that's not the point. The point for you here is to watch the video, but also read the step by step
+through a problem and see how the interviewee works on the problem.
+
+In this case in that video follows the steps on the "Step By Step through a problem" section on your cheatsheet PDF.
+And those steps are what you're going to do when you solve a problem in a coding interview. The fun thing is that at
+the end of this course, we're going to revisit the video, and you're going to notice a difference from this very first
+time that you're going to watch the video to the very end where what the interviewee does becomes second nature.
+
+You understand what they're coding in, what they're doing to solve the problem and all these little steps. So that
+you succeed if you completely get this video, and you understand how to run a successful interview at a company like
+Google, or any large company, or maybe even startup, depending on what you like.
+"""
